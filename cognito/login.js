5f8f0646b84c423002dfbfe52305a710edb1421a
msanchis2 <martisanchis2000@gmail.com>
2022-05-10 15:37:32 +0200

    inicio landing centros

--- cognito/login.js
+++ cognito/login.js
@@ -1,49 +1,49 @@
-var poolData;
+// var poolData;
 
-function loginButton() {
-    var authData = {
-        Username : document.getElementById("loginEmail").value,
-        Password : document.getElementById("loginPwd").value
-    };
+// function loginButton() {
+//     var authData = {
+//         Username : document.getElementById("loginEmail").value,
+//         Password : document.getElementById("loginPwd").value
+//     };
 
-    var authDetails = new AmazonCognitoIdentity.AuthenticationDetails(authData);
+//     var authDetails = new AmazonCognitoIdentity.AuthenticationDetails(authData);
 
-    poolData = {
-        UserPoolId : _config.cognito.userPoolId,
-        ClientId : _config.cognito.clientId
-    };
+//     poolData = {
+//         UserPoolId : _config.cognito.userPoolId,
+//         ClientId : _config.cognito.clientId
+//     };
 
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+//     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-    var userData = {
-        Username : document.getElementById("loginEmail").value,
-        Pool : userPool
-    };
+//     var userData = {
+//         Username : document.getElementById("loginEmail").value,
+//         Pool : userPool
+//     };
 
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+//     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
-    cognitoUser.authenticateUser(authDetails, {
-        onSuccess: function (result) {
-            //var accessToken = result.getAccessToken().getJwtToken();
-            var currentUser = userPool.getCurrentUser();
+//     cognitoUser.authenticateUser(authDetails, {
+//         onSuccess: function (result) {
+//             //var accessToken = result.getAccessToken().getJwtToken();
+//             var currentUser = userPool.getCurrentUser();
 
-            currentUser.getSession(function (err, session) {
-                console.log('Access token: ' + session.accessToken.jwtToken);
-                console.log('Refresh token: ' + session.refreshToken.token);
-                currentUser.getUserAttributes(function (err, result) {
-                    if (err) {
-                        alert(err.message || JSON.stringify(err));
-                        return;
-                    }
-                    for (i = 0; i < result.length; i++) {
-                        console.log('Atributo ' + result[i].getName() + ' con valor: ' + result[i].getValue());
-                    }
-                });
-            });
-        },
+//             currentUser.getSession(function (err, session) {
+//                 console.log('Access token: ' + session.accessToken.jwtToken);
+//                 console.log('Refresh token: ' + session.refreshToken.token);
+//                 currentUser.getUserAttributes(function (err, result) {
+//                     if (err) {
+//                         alert(err.message || JSON.stringify(err));
+//                         return;
+//                     }
+//                     for (i = 0; i < result.length; i++) {
+//                         console.log('Atributo ' + result[i].getName() + ' con valor: ' + result[i].getValue());
+//                     }
+//                 });
+//             });
+//         },
 
-        onFailure: function (err) {
-            alert(err.message || JSON.stringify(err));
-        },
-    });
-}
+//         onFailure: function (err) {
+//             alert(err.message || JSON.stringify(err));
+//         },
+//     });
+// }
