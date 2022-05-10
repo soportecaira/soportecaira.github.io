14c15a1b4c530096301c91f3dac569a956bfb283
msanchis2 <martisanchis2000@gmail.com>
2022-05-10 10:44:22 +0200

    adicion de partners

--- main.js
+++ main.js
@@ -42,14 +42,14 @@ let partners = [
     ["ada.jpg","bcas.png","isam.jpg","trepcamp.png"],
     ["4geeks.png","assembler.jpg","hult.png","ironhack.jpg"],
     ["retail.png","siena.png","sige.png","techma.png"],
-    ["unavir.jpg","Iso gradiente.png","Iso gradiente.png","Iso gradiente.png"]
+    ["unavir.jpg","elingua.png","lunar.jpg","Iso gradiente.png"]
 ];
 
 let enlances = [ 
     ["https://ada-school.org/","https://bcasapp.com/","https://isam.education/","https://www.trepcamp.org/"],
     ["https://4geeksacademy.com/es/inicio","https://assemblerinstitute.com/","https://www.hult.edu/","https://www.ironhack.com/en"],
     ["https://retail-institute.org/","https://www.siena.edu/programs/education/","https://www.sige.es/","https://techmabs.com/"],
-    ["https://www.facebook.com/Unavir-108107404246902/","","",""]
+    ["https://www.facebook.com/Unavir-108107404246902/","https://elingua.es/","https://lunarxy.com/",""]
 ];
 
 setInterval(function(){
