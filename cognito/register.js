83a37c46dcd4a1210ab9dc016622fc6ed8474dd7
jcpicot04 <jcpicot1718@gmail.com>
2022-05-11 17:25:53 +0200

    toast and regex validations added for register form

--- cognito/register.js
+++ cognito/register.js
@@ -2,16 +2,67 @@ var userName;
 var email;
 var password;
 var poolData;
+var message;
+var regexSpecialCharacters;
+var userpwd;
 
 function registerButton() {
-    userName = document.getElementById("username").value + ' ' + document.getElementById("usersurname").value;
     email = document.getElementById("useremail").value;
+    userpwd = document.getElementById("userpwd");
+    message = document.getElementById("errorMessage");
+    regexSpecialCharacters = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-“!@#%&/,><\’:;|_~`])\S{8,99}$/;
 
-    if(document.getElementById("userpwd").value != document.getElementById("userpwdconf").value){
-        console.log("Passwords don't match.");
-        throw "Passwords don't match.";
+
+    if(!document.getElementById("username").value || document.getElementById("username").length === 0 ){
+        message.style.display="block";
+        message.innerHTML="The name cannot be empty.";
+        return false;
+    } else if(!document.getElementById("usersurname").value || document.getElementById("usersurname").length === 0 ) {
+        message.style.display="block";
+        message.innerHTML="The surname cannot be empty.";
+        return false;
+    }else {
+        userName = document.getElementById("username").value + ' ' + document.getElementById("usersurname").value;
+    }
+
+    if(userpwd.value != document.getElementById("userpwdconf").value){
+        message.style.display="block";
+        message.innerHTML="Passwords don't match.";
+        return false;
     } else {
-        password = document.getElementById("userpwd").value;
+        password = userpwd.value;
+    }
+    
+    if(userpwd.value.length < 8){
+        message.style.display="block";
+        message.innerHTML="The password must contain at least 8 characters.";
+        return false;
+    } else {
+        password = userpwd.value;
+    }
+
+    if(userpwd.value.length > 15){
+        message.style.display="block";
+        message.innerHTML="The password must contain 15 characters maximum.";
+        return false;
+    } else {
+        password = userpwd.value;
+    }
+
+    if(!/\d/.test(userpwd.value)){
+        message.style.display="block";
+        message.innerHTML="The password must contain 1 number minimum.";
+        return false;
+    } else {
+        password = userpwd.value;
+    }
+
+    if(!regexSpecialCharacters.test(userpwd.value)){
+        message.style.display="block";
+        message.innerHTML="The password must contain at least 1 special character.";
+        return false;
+    } else {
+        password = userpwd.value;
     }
 
     poolData = {
@@ -39,6 +90,8 @@ function registerButton() {
     attributeList.push(attributeEmail);
     attributeList.push(attributePersonalName);
 
+    event.preventDefault();
+
     userPool.signUp(email, password, attributeList, null, function(err, result){
         if (err) {
             console.log(err.message || JSON.stringify(err));
@@ -46,7 +99,13 @@ function registerButton() {
         }
 
         cognitoUser = result.user;
-        console.log('User: ' + cognitoUser.getUsername());
-        alert('Check your email for a verification link');
+
+        new Notify ({
+            title: 'Registro efectuado',
+            text: 'Te hemos enviado un correo para verificar tu cuenta.',
+            status: 'success'
+        })
     });
+
+    return true;
 }
