7a60fce654c27e1ccd4a0ca200935df97a9795cc
jcpicot04 <jcpicot1718@gmail.com>
2022-05-23 12:44:44 +0200

    Registed page logic fixes

--- js/registerGoogle.js
+++ js/registerGoogle.js
@@ -1,22 +1,33 @@
 var hash = window.location.hash.substr(1);
-console.log("Hash: " + hash);
-var result = hash.split('&').reduce(function (res, item) {
-    var parts = item.split('=');
-    res[parts[0]] = parts[1];
-    return res;
-}, {});
-console.log("Result: " + result);
-var base64Url = result.id_token.split('.')[1];
-var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-}).join(''));
-
-var data = JSON.parse(jsonPayload);
-console.log("Data: " + data);
-
 let message = document.getElementById("messageRegistered");
 
-result.username ? message.innerHTML= "Thank you for registering " + result.username : message.innerHTML= "Thank you for registering " + data.email;
+console.log("Hash: " + hash);
+var result = hash.split("&").reduce(function (res, item) {
+  var parts = item.split("=");
+  res[parts[0]] = parts[1];
+  return res;
+}, {});
 
+console.log("Result: " + result.toString());
 
+if (result.id_token) {
+  var base64Url = result.id_token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  var data = JSON.parse(jsonPayload);
+
+  message.innerHTML = "Thank you for registering " + data.email;
+}
+
+console.log("Data: " + data.toString());
+
+result.username &&
+  message.innerHTML == "Thank you for registering " + result.username;
